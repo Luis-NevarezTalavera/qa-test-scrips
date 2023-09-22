128e3dbec118e118307f984608a147583be9572c
Luis Nevarez <LNevarezTalavera@eagle.org>
2023-09-22 14:25:47 +0000

    logs_check-errors

--- logs_check-errors.bat
+++ logs_check-errors.bat
@@ -1,10 +1,10 @@
 echo off
 cd C:\ABS\TestClient\LogFolder-%1-%2\
 echo Looking for Warning or Error log entries in the CC-DISCO.log and Siemens-Adapter.log files
-echo +++ CC-DISCO +++ 1>  WarningError.log
-grep -E '"Warning"' CC-DISCO.log >> WarningError.log
-grep -E '"Error"'   CC-DISCO.log >> WarningError.log
-echo +++ Siemens-Adapter +++ 1>>  WarningError.log
-grep -E '"Warning"' Siemens-Adapter.log >> WarningError.log
-grep -E '"Error"'   Siemens-Adapter.log >> WarningError.log
+echo +++ TestRun-%1-%2 +++ 1>  Warnings-Errors.log
+grep -E "Warning|Error|BAD_REQUEST" TestRun-%1-%2.log  | grep -v "CleanupTrackedErrors" >> Warnings-Errors.log
+echo +++ CC-DISCO +++ 1>>  Warnings-Errors.log
+grep -E "Warning|Error|BAD_REQUEST" CC-DISCO.log       | grep -v "CleanupTrackedErrors" >> Warnings-Errors.log
+echo +++ Siemens-Adapter +++ 1>>  Warnings-Errors.log
+grep -E '"Warning|Error|BAD_REQUEST"' Siemens-Adapter.log  | grep -v "CleanupTrackedErrors" >> Warnings-Errors.log
 cd ..
