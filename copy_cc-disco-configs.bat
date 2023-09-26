328f38965ff1c0cb22d1af31ab5f1c13b5fc5425
Luis Nevarez <LNevarezTalavera@eagle.org>
2023-09-26 22:16:19 +0000

    updates

--- copy_cc-disco-configs.bat
+++ copy_cc-disco-configs.bat
@@ -3,3 +3,4 @@ cd c:\ABS\cc-disco\
 docker cp service\ServiceBusConfigurator.Tests\TestData\ValidConfigs\vendor-configs\siemens-config.json %1:/app/config/vendor-configs
 timeout 25 1>NUL
 docker cp client\TestClient\DiscoClientConfig\test-client-config.json %1:/app/config/client-configs
+cd c:\ABS\TestClient\
