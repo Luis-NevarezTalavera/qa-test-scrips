f6aff23cd602b270aae9b2f34ec78e3d7391cceb
Luis Nevarez <LNevarezTalavera@eagle.org>
2023-09-27 22:20:28 +0000

    set-Variables_ABS-Nuget.bat

--- test-client_update.bat
+++ test-client_update.bat
@@ -1,5 +1,4 @@
 echo Updating DISCO Test Client
 cd c:\ABS\TestClient\
-set ABS_NUGET_USERNAME=Luis-NevarezTalavera
-set /p ABS_NUGET_PASSWORD=<c:\Users\LNevarezTalavera\myGitHubToken.txt
+setVariables_ABS-Nuget.bat LNevarezTalavera
 dotnet tool update Abs.CommonCore.Disco.TestClient --global --configfile c:\ABS\TestClient\nuget.config
