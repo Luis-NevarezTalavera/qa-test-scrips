d82601fd4cd81efb36a488e295d409d4fe5df4c6
Luis Nevarez <LNevarezTalavera@eagle.org>
2023-09-27 19:24:35 +0000

    test scripts

--- copy_cc-disco-configs.bat
+++ copy_cc-disco-configs.bat
@@ -1,6 +1,6 @@
 echo "Copying configs into container "%1
 cd c:\ABS\cc-disco\
 docker cp service\ServiceBusConfigurator.Tests\TestData\ValidConfigs\vendor-configs\siemens-config.json %1:/app/config/vendor-configs
-timeout 25 1>NUL
+:: timeout 25 1>NUL
 docker cp client\TestClient\DiscoClientConfig\test-client-config.json %1:/app/config/client-configs
 cd c:\ABS\TestClient\
