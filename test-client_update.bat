4ffe5230a01e31e32caab2b4582ea47abbfdc03a
Luis Nevarez <LNevarezTalavera@eagle.org>
2023-09-27 21:06:30 +0000

    nuget.config from c:\ABS\TestClient\

--- test-client_update.bat
+++ test-client_update.bat
@@ -1,3 +1,3 @@
 echo Updating DISCO Test Client
 cd c:\ABS\TestClient\
-dotnet tool update Abs.CommonCore.Disco.TestClient --global --configfile nuget.config
+dotnet tool update Abs.CommonCore.Disco.TestClient --global --configfile c:\ABS\TestClient\nuget.config
