25874a23fd7a299cb733e9f56d04b201e5f23273
Luis Nevarez <LNevarezTalavera@eagle.org>
2023-10-03 19:58:29 +0000

    logs_(check-errors, get-summary), test-client_exec

---
+++ logs_get-summary.bat
@@ -0,0 +1,39 @@
+echo off
+cd C:\ABS\TestClient\LogFolder-%1-%2\
+echo Starting Summary for Test: %1-%2
+echo ++--- Summary for Test: %1-%2 ---++ > Summary_%1-%2.log
+echo Start Date-Time: %DateTime% >> Summary_%1-%2.log
+echo ----------------------------------------------------------------------- >> Summary_%1-%2.log
+echo. >> Summary_%1-%2.log
+
+echo | set /p="Handling files: " >> Summary_%1-%2.log
+grep -c "Handling file" TestRun-%1-%2.log >> Summary_%1-%2.log
+echo. >> Summary_%1-%2.log
+
+echo --- Request / Responses --- >> Summary_%1-%2.log
+echo | set /p="DiscoCreateScheduleRequests: " >> Summary_%1-%2.log
+grep -c "Created DiscoCreateScheduleRequest" TestRun-%1-%2.log >> Summary_%1-%2.log
+echo | set /p="DiscoScheduleResponses: " >> Summary_%1-%2.log
+grep -c "DiscoScheduleResponse Received" TestRun-%1-%2.log >> Summary_%1-%2.log
+echo. >> Summary_%1-%2.log
+
+echo | set /p="DiscoDataRequests: " >> Summary_%1-%2.log
+grep -c "Created DiscoDataRequest" TestRun-%1-%2.log >> Summary_%1-%2.log
+echo | set /p="DiscoDataResponse: " >> Summary_%1-%2.log
+grep -c "DiscoDataResponse Received" TestRun-%1-%2.log >> Summary_%1-%2.log
+echo. >> Summary_%1-%2.log
+
+echo -- BAD_REQUESTs / Warnings / Errors --- >> Summary_%1-%2.log
+echo | set /p="TestRun.log BAD_REQUEST: "  >> Summary_%1-%2.log
+grep -E -c "BAD_REQUEST" TestRun-%1-%2.log >> Summary_%1-%2.log
+echo | set /p="TestRun.log Warnings, Errors: " >> Summary_%1-%2.log
+grep -E "Warning|Error" TestRun-%1-%2.log     | grep -v -E "BAD_REQUEST|CleanupTrackedErrors" | grep -E -c "Warning|Error" >> Summary_%1-%2.log
+echo | set /p="disco-service.log Warnings, Errors: " >> Summary_%1-%2.log
+grep -E    "Warning|Error"  disco-service*.log   | grep -v "CleanupTrackedErrors" | grep -E -c "Warning|Error" >> Summary_%1-%2.log
+echo | set /p="siemens-adapter.log Warnings, Errors: " >> Summary_%1-%2.log
+grep -E    "Warning|Error"  siemens-adapter*.log | grep -v "CleanupTrackedErrors" | grep -E -c "Warning|Error" >> Summary_%1-%2.log
+echo. >> Summary_%1-%2.log
+
+echo ----------------------------------------------------------------------- >> Summary_%1-%2.log
+echo End Date-Time: %DATE% %TIME% >> Summary_%1-%2.log
+cd ..
--- test-client_execute.bat
+++ test-client_execute.bat
@@ -1,10 +1,11 @@
 @echo off
 get_date-time.bat | awk -F ':' '{print $1}' > _temp_DateHour.txt
 set /p DateHour=<_temp_DateHour.txt
-echo Executing test client, test cases from folder TestClientRequests\%1
+echo ++--- Executing test client, test cases from folder TestClientRequests\%1 --++
 echo saving logs to LogFolder-%1-%2_%DateHour%
 cd c:\ABS\TestClient\
-time /t
+set DateTime=%DATE% %TIME%
+echo Start Date, Time: %DateTime%
 echo on
 disco-test-client TestClientRequests\%1 C:\ABS\TestClient\ConnectionConfig.json c:\ABS\TestClient\LogFolder-%1-%2_%DateHour%
 echo off
@@ -13,5 +14,6 @@ cd LogFolder-%1-%2_%DateHour%\
 copy C:\ABS\cc-platform\logs\site\disco-service-%DateHour%.log
 copy C:\ABS\cc-platform\logs\site\siemens-adapter-%DateHour%.log
 copy TestRun.log TestRun-%1-%2_%DateHour%.log
-cd ..
 logs_check-errors.bat %1 %2_%DateHour%
+logs_get-summary.bat %1 %2_%DateHour%
+cd ..
