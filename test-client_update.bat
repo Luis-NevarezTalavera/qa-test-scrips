4b7b9a87d22821a6d62e56d4bde10c7efb1e2382
Luis Nevarez <LNevarezTalavera@eagle.org>
2023-10-04 15:29:54 +0000

    test-client_update set-Variables

--- test-client_update.bat
+++ test-client_update.bat
@@ -1,5 +1,5 @@
 echo off
 echo Updating DISCO Test Client
 cd c:\ABS\TestClient\
-:: set-Variables_ABS-Nuget.bat LNevarezTalavera
+set-Variables_ABS-Nuget.bat LNevarezTalavera
 dotnet tool update Abs.CommonCore.Disco.TestClient --global --configfile c:\ABS\TestClient\nuget.config
