6133bc4246a8b2a7021a650c72cf883d770462e3
Luis Nevarez <LNevarezTalavera@eagle.org>
2023-10-05 20:12:46 +0000

    docker-rm_all-images

---
+++ docker-rm_all-images.bat
@@ -0,0 +1,14 @@
+@echo off
+cd c:\ABS\TestClient\
+
+echo ++++ Current Docker Images ++++
+docker image ls
+echo.
+echo ++++ Removing all Docker Images ++++
+docker rmi --force ghcr.io/abs-wavesight/cc-adapters-siemens:windows-2019
+docker rmi --force ghcr.io/abs-wavesight/cc-disco:windows-2019
+docker rmi --force ghcr.io/abs-wavesight/vector:windows-2019
+:: docker rmi --force ghcr.io/abs-wavesight/rabbitmq:windows-2019
+echo.
+echo ++++ ABS CC Disco, Siemens-Adapter and Vector Images Removed ... Images that still exists are: ++++
+docker image ls
