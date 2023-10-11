37d1f8e0f5b4ed577375b311b7213a4bef260ac5
Luis Nevarez <LNevarezTalavera@eagle.org>
2023-10-11 17:17:55 +0000

    logs_check-errors, get-summary

--- logs_check-errors.bat
+++ logs_check-errors.bat
@@ -5,6 +5,7 @@ echo +++ Errors Report for: %2 +++ 1>  %LogFolder%\Warnings-Errors.log
 echo Start Date-Time: %StartDateTime%  >> %LogFolder%\Warnings-Errors.log
 echo ----------------------------------------------------------------------- >> %LogFolder%\Warnings-Errors.log 
 echo. >> %LogFolder%\Warnings-Errors.log
+echo +++ TestRun_%2.log +++ 1>>  %LogFolder%\Warnings-Errors.log
 grep -E "Warning|Error|BAD_REQUEST" %LogFolder%\TestRun_%2.log    | grep -v "CleanupTrackedErrors" >> %LogFolder%\Warnings-Errors.log
 echo +++ disco-service +++ 1>>  %LogFolder%\Warnings-Errors.log
 grep -E "Warning|Error|fail:|BAD_REQUEST" %LogFolder%\disco-service_%2.log   | grep -v "CleanupTrackedErrors" >> %LogFolder%\Warnings-Errors.log
