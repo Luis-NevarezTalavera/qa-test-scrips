0b766dfe23f32e56d3516a808f5dbc024ef1bcfe
Luis Nevarez <LNevarezTalavera@eagle.org>
2023-10-13 23:11:05 +0000

    loadtest-create_test-clients-configs

--- loadtest-create_test-clients-configs.bat
+++ loadtest-create_test-clients-configs.bat
@@ -3,10 +3,10 @@ echo +++--- Creating Test Clients qty: %1 ---+++
 cd C:\ABS\TestClient\LoadTest\
 
 echo Creating test-client-xx folders, qty: %1\
-for /l %%i in (1,1,%1) do mkdir test-client-%%i
+for /l %%i in (1,1,%1) do mkdir disco-test-client-%%i
 
 echo Populating requests
-for /l %%i in (1,1,%1) do loadtest-populate_test-client-folder.bat test-client-%%i
+for /l %%i in (1,1,%1) do loadtest-populate_test-client-folder.bat disco-test-client-%%i
 
 echo Copying CC-Disco Configs (Creating Exchanges, Queues)
-for /l %%i in (1,1,%1) do copy_cc-disco-configs.bat test-client-%%i
+for /l %%i in (1,1,%1) do copy_cc-disco-configs.bat disco-test-client-%%i
--- loadtest-populate_test-client-folder.bat
+++ loadtest-populate_test-client-folder.bat
@@ -3,11 +3,13 @@ echo +++--- Duplicating Requests for Client: %1 ---+++
 cd C:\ABS\TestClient\LoadTest\
 
 echo Copying CC-Disco Config Files
-xcopy /f /q /v /y test-client\siemens-config.json %1\
-xcopy /f /q /v /y test-client\ConnectionConfig.json %1\
-echo F|xcopy /f /q /v /y test-client\test-client-config.json %1\%1-config.json
+xcopy /f /q /v /y disco-test-client\siemens-config.json %1\
+xcopy /f /q /v /y disco-test-client\ConnectionConfig.json %1\
+echo F|xcopy /f /q /v /y disco-test-client\disco-test-client-config.json %1\%1-config.json
 cd %1
-sed -i 's/test-client/%1/g' *.json
+sed -i 's/disco-test-client/%1/g' *.json
+sed -i 's/Username\": \"%1/Username\": \"disco-test-client/g' ConnectionConfig.json
+sed -i 's/Password\": \"%1/Password\": \"disco-test-client/g' ConnectionConfig.json
 cd ..
 dir /b test-client\*. > test-client\requestfolders.txt
-for /f %%i in (test-client\requestfolders.txt) do loadtest-create_request-folder.bat %1 %%i
+for /f %%i in (disco-test-client\requestfolders.txt) do loadtest-create_request-folder.bat %1 %%i
