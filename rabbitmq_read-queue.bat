627ff443f5a89794dce4f9a4dcfae8967f4f6969
Luis Nevarez <LNevarezTalavera@eagle.org>
2023-10-27 14:04:10 +0000

    test-client_set-time

--- rabbitmq_read-queue.bat
+++ rabbitmq_read-queue.bat
@@ -1,3 +1,3 @@
 @echo off
 echo --- Getting localhost Rabbit-MQ messages from queue: %2 logged as user: %1 max qty of messages to read: %3 ---
-buneary get messages localhost %2 --user %1 --password %1 --max %3
+echo y | buneary get messages localhost %2 --user %1 --password %1 --max %3
