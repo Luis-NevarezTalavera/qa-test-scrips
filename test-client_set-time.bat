92bde143318cbfa4d4c3f530ddb9ee8dde4ed2b6
Luis Nevarez <LNevarezTalavera@eagle.org>
2023-11-14 22:53:07 +0000

    loadtest start, exec testclient, logs set summary

--- test-client_set-time.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set TestClient=%1
-set /a TimeOutSecs=%2*60
-echo +++--- Setting Automatic Timeout for Client: %1 to %TimeOutSecs% secs ( %2 minutes ) ---+++
-cd C:\ABS\TestClient\LoadTest\
-
-sed -i 's/"CloseAppTimeOutSeconds": 180/"CloseAppTimeOutSeconds": %TimeOutSecs%/g' %TestClient%\ConnectionConfig.json
